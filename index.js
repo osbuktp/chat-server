b50a3fc504b79b1a7013f5520041108a0d55ca70
osbuktp <os.buktp@yandex.ru>
2018-11-25 23:10:47 +0300

    lul

--- index.js
+++ index.js
@@ -92,3 +92,7 @@ io.on('connection', socket => {
     .on('message', (room, msg) => message(socket, room, msg))
     .on('disconnect', () => disconnect(socket))
 })
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
