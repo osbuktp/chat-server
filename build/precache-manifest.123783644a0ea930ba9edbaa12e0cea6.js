a98efc04fe2f1b500fdb0aeb2aa3635f6c7e29c7
osbuktp <os.buktp@yandex.ru>
2018-11-25 23:08:21 +0300

    lul

---
+++ build/precache-manifest.123783644a0ea930ba9edbaa12e0cea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "313e027f01acb91cf5af",
+    "url": "/static/js/main.313e027f.chunk.js"
+  },
+  {
+    "revision": "5e03fab98764c48f1812",
+    "url": "/static/js/1.5e03fab9.chunk.js"
+  },
+  {
+    "revision": "313e027f01acb91cf5af",
+    "url": "/static/css/main.8ec8f3c3.chunk.css"
+  },
+  {
+    "revision": "269e4eef539486eb0f42d5cdeade1469",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -1,42 +1,94 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server)
-
-let messageHistory = {}
-
-io.on('connection', client => {
-    client.currentRoom = client.id;
-    messageHistory[client.currentRoom] = [];
-    console.log(`Client connected: ${client.id}`);
-    client
-        // .on('register', handleRegister)
-        // .on('join', handleJoin)
-        // .on('leave', handleLeave)
-        .on('message', message => handleMessage(message, client))
-        .on('disconnect', () => {
-            console.log(`Client ${client.id} has disconnected`)
-            // handleDisconnect()
-        })
-        .on('error', err => {
-            console.log(`Error from ${client.id}: ${err}`)
-        })
-        .on('join', (room) => {
-            console.log(room)
-            client.currentRoom = room;
-            client.emit('message', `${client.rooms[room]}`)
-        })
+const static = require('node-static')
+const file = new static.Server('./build')
+const server = require('http').createServer((req, res) => {
+    file.serve(req, res)
 })
 
-function handleMessage(message, client) {
-    messageHistory[client.currentRoom].push({
-        message,
-        id: client.id,
+const io = require('socket.io')(server)
+
+let rooms = {}
+let userRooms = {}
+let users = {}
+
+function sendMessage(room, message) {
+    io.in(room).emit('message', room, 0, {
+        text: message,
         timestamp: new Date()
-    });
-    let msg = `${client.id}: ${message}`;
-    io.in(client.currentRoom).emit('message', msg)
+    })
 }
 
-server.listen(3030, err => {
-    if (err) throw err
-    console.log('Listening on port 3030')
+function disconnect(socket) {
+    console.log(`${socket.id} has disconnected`)
+    io.in(socket.id).emit('closed')
+    userRooms[socket.id].forEach(room => {
+        if (rooms[room]) {
+            rooms[room].users.delete(socket.id)
+            io.in(room).emit('user-left', socket.id)
+            sendMessage(room, `User ${socket.name} has left the room`)
+        }
+    })
+    delete rooms[socket.id]
+}
+function signal(socket, toId, msg) {
+    io.to(toId).emit('signal', socket.id, msg)
+}
+function joinRoom(socket, room) {
+    socket.join(room)
+    console.log(`${socket.id} joined ${room}`)
+    if (!rooms[room]) {
+        console.log('No such room')
+        return
+    }
+    userRooms[socket.id].add(room)
+    rooms[room].users.add(socket.id)
+    io.in(room).emit('user-joined', room, socket.id)
+    sendMessage(room, `User ${socket.name} has joined the room`)
+    socket.emit('joined', room)
+}
+function message(socket, room, msg) {
+    io.in(room).emit('message', room, socket.name, {
+        text: msg,
+        timestamp: new Date()
+    })
+}
+function joinTranslation(socket, room) {
+    console.log(`${socket.id} has joined video translation in room ${room}`)
+    rooms[room].translation.add(socket.id)
+    io.in(room).emit('user-joinedTranslation', socket.id, rooms[room].translation.size, Array.from(rooms[room].translation))
+}
+function leaveTranslation(socket, room) {
+    console.log(`${socket.id} has left video translation in room ${room}`)
+    rooms[room].translation.delete(socket.id)
+    io.in(room).emit('user-leftTranslation', socket.id)
+}
+function register(socket, name) {
+    console.log(`Register: ${socket.id}:${name}`)
+    users[socket.id] = name
+    socket.name = name
+    socket.emit('registered', socket.id)
+}
+function chatInfo(socket, room) {
+    if (!rooms[room]) return
+    socket.emit('chat-info', {
+        chatUsers: Array.from(rooms[room].users).map(user => users[user]),
+        translationUsers: Array.from(rooms[room].translation).map(user => users[user])
+    })
+}
+
+io.on('connection', socket => {
+    console.log(`${socket.id} joined`)
+    if (!rooms[socket.id]) rooms[socket.id] = {
+        users: new Set(),
+        translation: new Set()
+    }
+    if (!userRooms[socket.id]) userRooms[socket.id] = new Set()
+    socket
+    .on('register', name => register(socket, name))
+    .on('signal', (toId, msg) => signal(socket, toId, msg))
+    .on('join-room', room => joinRoom(socket, room))
+    .on('get-chat-info', room => chatInfo(socket, room))
+    .on('join-translation', room => joinTranslation(socket, room))
+    .on('leave-translation', room => leaveTranslation(socket, room))
+    .on('message', (room, msg) => message(socket, room, msg))
+    .on('disconnect', () => disconnect(socket))
 })
